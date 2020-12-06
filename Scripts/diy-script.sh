44b95bcb1ebfbb82ff1964d4add2754f55a1328a
alfred600 <33020575+alfred600@users.noreply.github.com>
2020-12-06 13:48:37 +0800

    Update diy-script.sh

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -32,11 +32,12 @@ ExtraPackages svn other smartdns https://github.com/project-openwrt/openwrt/trun
 ExtraPackages git other OpenClash https://github.com/vernesong master
 ExtraPackages git other luci-app-serverchan https://github.com/tty228 master
 ExtraPackages svn other luci-app-socat https://github.com/project-openwrt/openwrt/trunk/package/lienol
+#alfred600
+#ExtraPackages git lienol https://github.com/Lienol/openwrt-package master
 # [UPX 压缩] ExtraPackages git other openwrt-upx https://github.com/Hyy2001X master
 # [应用过滤] ExtraPackages git OAF openwrt-OpenAppFilter https://github.com/Lienol master
+ExtraPackages git OAF openwrt-OpenAppFilter https://github.com/Lienol master
 # [AdGuardHome 核心] ExtraPackages svn other AdGuardHome https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
-#alfred600
-#ExtraPackages git lienol https://github.com/Lienol/openwrt-package.git master
 }
 
 Diy-Part2() {
